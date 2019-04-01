efae3312ddf3010f2dae449bac10b877fd7306d6
runningiris <irisrunning@foxmail.com>
2019-04-01 18:19:59 +0800

    convert tabs to spaces

--- src/Deque.java
+++ src/Deque.java
@@ -1,112 +1,115 @@
 import edu.princeton.cs.algs4.StdOut;
+
 import java.util.NoSuchElementException;
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-	
-	private Node first, last;
-	
-	private class Node {
-		Item item;
-		Node next;
-	}
-	
-	public boolean isEmpty() {
-		return first == null;
-	}
-	
-	public int size() {
-		int len = 0;
-		while (removeLast() != null) {
-			len++;
-		}
-		return len;
-	}
-	
-	// add the item to the front
-	public void addFirst(Item item) {
-		if (item == null) {
-			throw new IllegalArgumentException("argument item should not be null.");
-		}
-		Node node = new Node();
-		node.item = item;
-		if (isEmpty()) {
-			first = last = node;
-		} else {
-			Node oldFirst = first;
-			first = node;
-			first.next = oldFirst;
-		}
-	}
-	
-	public void addLast(Item item) {
-		if (item == null) {
-			throw new IllegalArgumentException("argument item should not be null.");
-		}
-		Node node = new Node();
-		node.item = item;
-		if (isEmpty()) {
-			first = last = node;
-		} else {
-			last.next = node;
-		}
-	}
 
-	public Item removeFirst() {
-		if (isEmpty()) {
-			throw new NoSuchElementException("no item to be removed in the deque");
-		}
+    private Node first, last;
 
-		Item item = first.item;
+    private class Node {
+        Item item;
+        Node next;
+    }
 
-		first = first.next;
+    public boolean isEmpty() {
+        return first == null;
+    }
 
-		return item;
-	}
-	
-	public Item removeLast() {
-		if (isEmpty()) {
-			throw new NoSuchElementException("no item to be removed in the deque");
-		}
-		
-		Node oldLast = last;
-		
-		if (oldLast.next != null) {
-			last = oldLast.next;
-		} else {
-			first = last = null;
-		}
-		return oldLast.item;		
-	}
-	public Iterator<Item> iterator() {
-		return new DequeIterator();
-	}
+    public int size() {
+        int len = 0;
+        while (removeLast() != null) {
+            len++;
+        }
+        return len;
+    }
 
-	private class DequeIterator implements Iterator<Item> {
-		private Node current = first;
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("argument item should not be null.");
+        }
+        Node node = new Node();
+        node.item = item;
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            Node oldFirst = first;
+            first = node;
+            first.next = oldFirst;
+        }
+    }
 
-		public boolean hasNext() {
-			return current != null;
-		}
-		public Item next() {
-			if (current == null) {
-				throw new NoSuchElementException("no more items to return in the next() method.");
-			}
-			Item item = current.item;
-			current = current.next;
-			return item;
-		}
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("argument item should not be null.");
+        }
+        Node node = new Node();
+        node.item = item;
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node;
+        }
+    }
 
-	}
+    public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("no item to be removed in the deque");
+        }
 
-	public static void main(String[] args) {
-		Deque<String> k = new Deque<String>();
-		k.addFirst("hha");
-		k.addFirst("haha");
-		k.addFirst("heihei");
-		k.addLast("heiha");
-		Iterator<String> iterator = k.iterator();
-		StdOut.println(iterator.next());
-	}
+        Item item = first.item;
+
+        first = first.next;
+
+        return item;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("no item to be removed in the deque");
+        }
+
+        Node oldLast = last;
+
+        if (oldLast.next != null) {
+            last = oldLast.next;
+        } else {
+            first = last = null;
+        }
+        return oldLast.item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if (current == null) {
+                throw new NoSuchElementException("no more items to return in the next() method.");
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Deque<String> k = new Deque<String>();
+        k.addFirst("hha");
+        k.addFirst("haha");
+        k.addFirst("heihei");
+        k.addLast("heiha");
+        Iterator<String> iterator = k.iterator();
+        StdOut.println(iterator.next());
+    }
 }
 
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,116 +1,124 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-	private Node first, last;
+    private Node first, last;
 
-	private class Node {
-		Item item;
-		Node next;
-	}
-	public boolean isEmpty() {
-		return first == null;
-	}
-	public int size() {
-		int len = 0;
-		Node current = first;
-		while(current != null) {
-			len++;
-			current = current.next;
-		}
-		return len;
-	}
-	public void enqueue(Item item) {
-		if (item == null) {
-			throw new IllegalArgumentException("item should not be null in method enqueue.");
-		}
-		Node newNode = new Node();
-		newNode.item = item;
+    private class Node {
+        Item item;
+        Node next;
+    }
 
-		if (isEmpty()) {
-			first = last = newNode;
-		} else {
-			last.next = newNode;
-			last = newNode;
-		}
-	}
-	public Item dequeue() {
-		if (first == null) {
-			throw new NoSuchElementException("no more items to return in the dequeue() method.");
-		}
-		Node oldFirst = first;
-		first = first.next;
-		return oldFirst.item;
-	}
+    public boolean isEmpty() {
+        return first == null;
+    }
 
-	public Item sample() {
-		if (first == null) {
-			throw new NoSuchElementException("no more items to return in the sample() method.");
-		}
+    public int size() {
+        int len = 0;
+        Node current = first;
+        while (current != null) {
+            len++;
+            current = current.next;
+        }
+        return len;
+    }
 
-		int len = size();
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("item should not be null in method enqueue.");
+        }
+        Node newNode = new Node();
+        newNode.item = item;
 
-		if (len == 1) {
-			Item item = first.item;
-			first = last = null;
-			return item;
-		}
+        if (isEmpty()) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+    }
 
-		int random = StdRandom.uniform(len);
+    public Item dequeue() {
+        if (first == null) {
+            throw new NoSuchElementException("no more items to return in the dequeue() method.");
+        }
+        Node oldFirst = first;
+        first = first.next;
+        return oldFirst.item;
+    }
 
-		if (random == 0) {
-			Item item = first.item;
-			first = first.next;
-			return item;
-		}
+    public Item sample() {
+        if (first == null) {
+            throw new NoSuchElementException("no more items to return in the sample() method.");
+        }
 
-		int i = 0;
+        int len = size();
+
+        if (len == 1) {
+            Item item = first.item;
+            first = last = null;
+            return item;
+        }
+
+        int random = StdRandom.uniform(len);
+
+        if (random == 0) {
+            Item item = first.item;
+            first = first.next;
+            return item;
+        }
+
+        int i = 0;
 
         Node prev = new Node();
         prev.next = first;
 
-        while(i < random) {
-			i++;
+        while (i < random) {
+            i++;
             prev = prev.next;
         }
 
-		Item item = prev.next.item;
-		prev.next = prev.next.next;
+        Item item = prev.next.item;
+        prev.next = prev.next.next;
 
         return item;
-	}
-	public Iterator<Item> iterator() {
-		return new RandomizedQueueIterator();
-	}
-	private class RandomizedQueueIterator implements Iterator<Item> {
-		private Node current = first;
+    }
 
-		public boolean hasNext() {
-			return current != null;
-		}
-		public Item next() {
-			if (current == null) {
-				throw new NoSuchElementException("no more items to return in the next() method.");
-			}
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
 
-			Item item = current.item;
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        private Node current = first;
 
-			current = current.next;
+        public boolean hasNext() {
+            return current != null;
+        }
 
-			return item;
-		}
-	}
-	public static void main(String[] args) {
-		RandomizedQueue<String> r = new RandomizedQueue<>();
-		r.enqueue("hello");
-		r.enqueue("kkk");
-		r.enqueue("iii");
-		r.enqueue("world");
+        public Item next() {
+            if (current == null) {
+                throw new NoSuchElementException("no more items to return in the next() method.");
+            }
+
+            Item item = current.item;
+
+            current = current.next;
+
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomizedQueue<String> r = new RandomizedQueue<>();
+        r.enqueue("hello");
+        r.enqueue("kkk");
+        r.enqueue("iii");
+        r.enqueue("world");
         StdOut.println(r.sample());
-		StdOut.println(r.sample());
-		StdOut.println(r.sample());
-		StdOut.println(r.sample());
-	}
+        StdOut.println(r.sample());
+        StdOut.println(r.sample());
+        StdOut.println(r.sample());
+    }
 }
